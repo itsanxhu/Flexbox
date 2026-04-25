16e5768844359cd915ea987369eb583457aa3e62
anshu <anshhufx@gmail.com>
2026-04-25 22:06:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,51 @@
 import { useState } from "react";
-import Header from "./Components/Header";
 
 const Flexbox = () => {
   const [align, setalign] = useState("");
   return (
     <div className="fullpage">
-      <Header />
-
       <div className="parent">
-        <div className="first">
-          <h2 className="flex justify-center items-center text-neutral-500 ">
-            justify-content
-          </h2>
-          <div className="main">
-            <div className="box-parent">
-              <div className={`box ${align}`}>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
+        <div className="one">
+          <h1 className="header border-b border-gray-200">FLEXBOX</h1>
+          <div className="first">
+            <h2 className="flex justify-center items-center text-neutral-500 ">
+              justify-content
+            </h2>
+            <div className="main">
+              <div className="box-parent">
+                <div className={`box ${align}`}>
+                  <div className="line1"></div>
+                  <div className="line2"></div>
+                  <div className="line3"></div>
+                </div>
               </div>
-            </div>
-            <div className="btns">
-              <a>
-                <p onClick={() => setalign("flex-start")}>flex-start</p>
-              </a>
-              <a>
-                <p onClick={() => setalign("flex-center")}>flex-center</p>
-              </a>
-              <a>
-                <p onClick={() => setalign("flex-end")}>flex-end</p>
-              </a>
-              <a>
-                <p onClick={() => setalign("flex-space-btw")}>space-between</p>
-              </a>
-              <a>
-                <p onClick={() => setalign("flex-space-around")}>
-                  space-around
-                </p>
-              </a>
+              <div className="btns">
+                <a>
+                  <p onClick={() => setalign("flex-start")}>flex-start</p>
+                </a>
+                <a>
+                  <p onClick={() => setalign("flex-center")}>flex-center</p>
+                </a>
+                <a>
+                  <p onClick={() => setalign("flex-end")}>flex-end</p>
+                </a>
+                <a>
+                  <p onClick={() => setalign("flex-space-btw")}>
+                    space-between
+                  </p>
+                </a>
+                <a>
+                  <p onClick={() => setalign("flex-space-around")}>
+                    space-around
+                  </p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="second">
-          <div className="credit">Made by itsanxhu</div>
+        <div className="two">
+          <div className="credit">Made by</div>
+          <div className="credit">itsanxhu</div>
         </div>
       </div>
     </div>
